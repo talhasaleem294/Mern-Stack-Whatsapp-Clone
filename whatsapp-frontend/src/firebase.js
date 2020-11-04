053bb96a2eefce9e0f523883f8476f8043abcaa0
talhasaleem294 <72187060+talhasaleem294@users.noreply.github.com>
2020-11-03 21:13:19 -0800

    Add files via upload

---
+++ whatsapp-frontend/src/firebase.js
@@ -0,0 +1,9 @@
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBDpxDnomlCzlL30llaw0zYgq0ku-jPNEI",
+//     authDomain: "whatsapp-mern-7e0b2.firebaseapp.com",
+//     databaseURL: "https://whatsapp-mern-7e0b2.firebaseio.com",
+//     projectId: "whatsapp-mern-7e0b2",
+//     storageBucket: "whatsapp-mern-7e0b2.appspot.com",
+//     messagingSenderId: "410083621193",
+//     appId: "1:410083621193:web:016ef67fdba82f897bc298"
+//   };
